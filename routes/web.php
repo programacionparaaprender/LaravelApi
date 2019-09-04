256d0018a68f681ff299309d8e8e6505a8942610
Luis Correa <alberto13711@gmail.com>
2019-09-03 20:44:27 -0500

    Cambios

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+$json = array('value1', 'value2');
+
+Route::get('/json', function () {
+    $json2 = array();
+    $json2 = json_encode($json);
+    return $json;
+});
